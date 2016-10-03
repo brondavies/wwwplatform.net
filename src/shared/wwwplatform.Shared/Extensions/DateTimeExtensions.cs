568990c9476c68772fade726d2f01adb57201ede
brondavies <git@brontech.com>
2016-10-02 20:46:15 -0500

    Fix tz cookie path, consolidate tz adjust code

---
+++ src/shared/wwwplatform.Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace wwwplatform.Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToTimezone (this DateTime value, int timezoneOffset)
+        {
+            return value.AddMinutes(timezoneOffset);
+        }
+
+        public static DateTime FromTimezone(this DateTime value, int timezoneOffset)
+        {
+            return value.AddMinutes(0 - timezoneOffset);
+        }
+    }
+}
