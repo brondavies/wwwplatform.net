bf55fb5d930f62485c6ed8ac12e092b9d61f8a06
brondavies <bron@brontech.com>
2021-01-17 21:45:18 -0600

    Automatically install SQL session state

---
+++ src/web/Migrations/202101180316163_InstallSqlState.cs
@@ -0,0 +1,29 @@
+﻿namespace wwwplatform.Migrations
+{
+    using System.Data.Entity.Migrations;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using wwwplatform.Models;
+
+    public partial class InstallSqlState : DbMigration
+    {
+        public override void Up()
+        {
+            RunAspnetRegSql();
+        }
+
+        private void RunAspnetRegSql()
+        {
+            var connectionstring = new ApplicationDbContext().Database.Connection.ConnectionString;
+            var framework = RuntimeEnvironment.GetRuntimeDirectory();
+            var filename = Path.Combine(framework, "aspnet_regsql.exe");
+            var args = $"-ssadd -sstype c -Q -C \"{connectionstring}\"";
+            Process.Start(filename, args).WaitForExit();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
